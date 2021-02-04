518cbd5949b41238b9b82566a1d6c0ab24c2ed9d
madmonsieur <evbelico@student.42.fr>
2021-02-04 20:21:40 +0100

    Pushing home app

---
+++ services/src/layouts/MainLayout.vue
@@ -0,0 +1,149 @@
+<template>
+  <q-layout view="lHh Lpr lFf" class="shadow-2 rounded-borders">
+    <q-header elevated class="header-bg">
+      <div class="row items-center">
+        <q-toolbar class="q-my-md">
+          <q-btn
+            flat
+            dense
+            round
+            icon="menu"
+            aria-label="Menu"
+            @click="leftDrawerOpen = !leftDrawerOpen"
+          />
+
+          <q-img src="icons/scarabee.png" style="max-width: 4rem" contain />
+          <q-toolbar-title class="header-font-color">
+            <a href="/">Scarabée</a>
+          </q-toolbar-title>
+
+          <div>v1.0.0</div>
+        </q-toolbar>
+      </div>
+      <div class="row items-center justify-center">
+        <q-tabs v-model="tab" class="text-theme" no-caps @click="printTab">
+          <q-tab name="matching" label="Matching" />
+          <q-tab name="forum" label="Forum" />
+          <q-tab
+            name="services"
+            label="Services"
+            to="/"
+            active-class="services-tab"
+          />
+        </q-tabs>
+      </div>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above>
+      <q-list>
+        <q-item-label header class="text-grey-8">
+          Essential Links
+        </q-item-label>
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts">
+import EssentialLink from 'components/EssentialLink.vue';
+
+const linksData = [
+  {
+    title: 'Docs',
+    caption: 'quasar.dev',
+    icon: 'school',
+    link: 'https://quasar.dev'
+  },
+  {
+    title: 'Github',
+    caption: 'github.com/quasarframework',
+    icon: 'code',
+    link: 'https://github.com/quasarframework'
+  },
+  {
+    title: 'Discord Chat Channel',
+    caption: 'chat.quasar.dev',
+    icon: 'chat',
+    link: 'https://chat.quasar.dev'
+  },
+  {
+    title: 'Forum',
+    caption: 'forum.quasar.dev',
+    icon: 'record_voice_over',
+    link: 'https://forum.quasar.dev'
+  },
+  {
+    title: 'Twitter',
+    caption: '@quasarframework',
+    icon: 'rss_feed',
+    link: 'https://twitter.quasar.dev'
+  },
+  {
+    title: 'Facebook',
+    caption: '@QuasarFramework',
+    icon: 'public',
+    link: 'https://facebook.quasar.dev'
+  },
+  {
+    title: 'Quasar Awesome',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev'
+  },
+  {
+    title: 'Login page !',
+    caption: "Hello here is the forum's login page",
+    icon: 'fa fa-save',
+    link: '/login'
+  }
+];
+
+import { defineComponent, ref } from '@vue/composition-api';
+
+export default defineComponent({
+  name: 'MainLayout',
+  components: { EssentialLink },
+  setup() {
+    const leftDrawerOpen = ref(false);
+    const essentialLinks = ref(linksData);
+    let tab = ref('services');
+
+    function printTab() {
+      console.log(tab.value);
+    }
+
+    return { leftDrawerOpen, essentialLinks, tab, printTab };
+  }
+});
+</script>
+
+<style>
+.header-bg {
+  background-color: #30304c;
+}
+
+.header-font-color,
+.header-font-color a {
+  color: #f2c98f;
+  text-decoration: none;
+  font-size: 3rem;
+  font-weight: 200;
+}
+
+.text-theme {
+  color: #f2c98f;
+}
+
+.services-tab {
+  background-color: #4f7a8e;
+}
+</style>
---
+++ services/src/pages/Login.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page>
+    <h2>Howdy people this is the LOGIN page</h2>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from '@vue/composition-api';
+
+export default defineComponent({
+  name: 'LoginPage',
+  setup() {}
+});
+</script>
